a9ac838759c8cc90613ede301494980d7e8de9ae
guiireal <guii_real@hotmail.com>
2024-11-07 14:35:03 -0300

    wip

--- src/config.js
+++ src/config.js
@@ -25,7 +25,7 @@ exports.ASSETS_DIR = path.resolve(__dirname, "..", "assets");
 exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
 
 // Timeout em milissegundos por evento (evita banimento).
-exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 300;
+exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
 
 // Plataforma de API's
 exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
