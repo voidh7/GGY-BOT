c1502ccde846d158d30a8bc3237ad0c00ccd781d
guiireal <guii_real@hotmail.com>
2025-01-12 21:58:39 -0300

    wip

--- src/config.js
+++ src/config.js
@@ -33,7 +33,7 @@ exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 700;
 exports.SPIDER_API_BASE_URL = "https://api.spiderx.com.br/api";
 
 // Obtenha seu token, criando uma conta em: https://api.spiderx.com.br.
-exports.SPIDER_API_TOKEN = "";
+exports.SPIDER_API_TOKEN = "seu_token_aqui";
 
 // Caso queira responder apenas um grupo específico, coloque o ID dele aqui (ex: 120363023799506419@g.us). Apenas para testes internos!
 exports.ONLY_GROUP_ID = "";
