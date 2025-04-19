0986c439d93aaaa5d8472a4dbc1c449e271d461c
guiireal <guii_real@hotmail.com>
2025-04-19 15:23:34 -0300

    wip

--- src/config.js
+++ src/config.js
@@ -11,11 +11,13 @@ exports.BOT_NAME = "Takeshi Bot";
 
 // Número do bot. Coloque o número do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.BOT_NUMBER = "559999999999";
+// Se o seu DDD não for de SP ou do Rio, não coloque o 9 antes do número.
+exports.BOT_NUMBER = "558112345678";
 
 // Número do dono do bot. Coloque o número do dono do bot
 // (apenas números, exatamente como está no WhatsApp).
-exports.OWNER_NUMBER = "559999999999";
+// Se o seu DDD não for de SP ou do Rio, não coloque o 9 antes do número.
+exports.OWNER_NUMBER = "5521950502020";
 
 // Diretório dos comandos
 exports.COMMANDS_DIR = path.join(__dirname, "commands");
