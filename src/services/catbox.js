4bd4703dc7e6f326a8b4ec019013ff5459af9657
guiireal <guii_real@hotmail.com>
2025-04-27 14:08:16 -0300

    feat(catbox): integrate node-catbox library for image uploads to CatBox service to simplify file handling and improve maintainability fix(catbox): replace manual file upload implementation with node-catbox methods for better error handling and code clarity

--- src/services/catbox.js
+++ src/services/catbox.js
@@ -1,41 +1,36 @@
-const { errorLog } = require("../utils/logger");
+/**
+ * Módulo responsável por fazer o upload de imagens para o CatBox.
+ * O CatBox é um serviço de hospedagem de arquivos.
+ *
+ * @author Dev Gui
+ */
+const { Catbox } = require("node-catbox");
+const fs = require("node:fs");
+const path = require("node:path");
+const { TEMP_DIR } = require("../config");
+const { getRandomName } = require("../utils");
 
 exports.catBoxUpload = async (buffer) => {
+  const catBox = new Catbox();
+
+  const tempPath = path.resolve(TEMP_DIR, getRandomName("png"));
+
+  fs.writeFileSync(tempPath, buffer);
+
   try {
-    const formData = new FormData();
-    formData.append("reqtype", "fileupload");
-    formData.append("userhash", "");
-
-    const blob = new Blob([buffer], { type: "image/png" });
-    formData.append(
-      "fileToUpload",
-      blob,
-      "e1c25e2f18430875d15fdcfbb14257e8.png"
-    );
-
-    const result = await fetch("https://catbox.moe/user/api.php", {
-      method: "POST",
-      headers: {
-        accept: "application/json",
-        "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
-        "cache-control": "no-cache",
-        "sec-ch-ua": '"Brave";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-        "sec-gpc": "1",
-        "x-requested-with": "XMLHttpRequest",
-        Referer: "https://catbox.moe/",
-        "Referrer-Policy": "same-origin",
-      },
-      body: formData,
+    const response = await catBox.uploadFile({
+      path: tempPath,
     });
 
-    return await result.text();
+    setTimeout(() => {
+      if (fs.existsSync(tempPath)) {
+        fs.unlinkSync(tempPath);
+      }
+    }, 1000 * 60 * 5);
+
+    return response;
   } catch (error) {
-    errorLog("Erro ao fazer upload da imagem no CatBox: ", error);
+    console.error("Erro ao fazer upload para o CatBox:", error);
     return null;
   }
 };
